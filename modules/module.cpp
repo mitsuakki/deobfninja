50aea29fa7d5412dc4108eacb3236391014cd185
Alexis Daugé <alexis.dauge2004@gmail.com>
2025-04-28 10:50:39 +0200

    useless atm

--- modules/module.cpp
+++
@@ -1,7 +0,0 @@
-#include "module.h"
-
-Module::Module(std::string name, ModuleCategory category, std::string description) {
-    this->name = name;
-    this->category = category;
-    this->description = description;
-}

f8740aa3cfbf9677e7f18d3919ec406cea112e1b
Alexis Daugé <alexis.dauge2004@gmail.com>
2025-06-13 12:46:06 +0200

    tokenizer

--- include/methods/instructions/mbasimplifier.hpp
+++ include/methods/instructions/mbasimplifier.hpp
@@ -8,20 +8,116 @@
 /**
  * @brief The MBASimplifier class implements the IDeobfuscationMethod interface
  * to simplify MBA instructions in a binary.
- * 
- * This class is part of the deobfuscation methods used to analyze and simplify
- * obfuscated binaries, specifically targeting MBA (Multi-Byte Arithmetic) instructions.
  */
 class MBASimplifier : public IDeobfuscationMethod
 {
 public:
     MBASimplifier();
 
+    bool loadPatternsFromCSV(const std::string& csvFilePath);
+
     /**
-     * @brief Executes the MBASimplifier deobfuscation process.
-     * This method simplifies the MBAS instructions in the binary.
+     * @brief Tokenizes an MBA expression and maps it to LLIL operations.
      */
+    std::vector<std::variant<int, std::vector<int>>> tokenizeAndMapToLLIL(const std::string& expr) const;
+
+    std::vector<std::tuple<
+        std::string, size_t,
+        BinaryNinja::LowLevelILInstruction,
+        BinaryNinja::LowLevelILInstruction,
+        BinaryNinja::LowLevelILInstruction
+    >> searchPatternsInFunction(const BinaryNinja::Ref<BinaryNinja::Function>& func) const;
+
+    size_t replaceObfuscatedWithSimple(
+        const BinaryNinja::Ref<BinaryNinja::LowLevelILFunction>& llil,
+        const BinaryNinja::LowLevelILInstruction& srcExpr,
+        const BinaryNinja::LowLevelILInstruction& leftExpr,
+        const BinaryNinja::LowLevelILInstruction& rightExpr);
+
     void execute(const BinaryNinja::Ref<BinaryNinja::AnalysisContext>& analysisContext) override;
+
+private:
+    std::vector<std::pair<std::string, std::string>> patterns;
+
+    /**
+     * @brief Maps a simple operation symbol (e.g., '+') to the corresponding BinaryNinja LLIL operation constant.
+     */
+    int mapSymbolToLLIL(const std::string& opSymbol) const {
+        if (opSymbol == "+") return LLIL_ADD;
+        if (opSymbol == "-") return LLIL_SUB;
+        if (opSymbol == "*") return LLIL_MUL;
+        if (opSymbol == "^") return LLIL_XOR;
+        if (opSymbol == "&") return LLIL_AND;
+        if (opSymbol == "|") return LLIL_OR;
+        if (opSymbol == "~") return LLIL_NOT;
+        if (opSymbol == "/") return LLIL_DIVU;
+        if (opSymbol == "%") return LLIL_MODU;
+        if (opSymbol == "<<" || opSymbol == "<<<") return LLIL_LSL;
+        if (opSymbol == ">>" || opSymbol == ">>>") return LLIL_LSR;
+        if (opSymbol == "==") return LLIL_CMP_E;
+        if (opSymbol == "!=") return LLIL_CMP_NE;
+        if (opSymbol == "<") return LLIL_CMP_ULT;
+        if (opSymbol == "<=") return LLIL_CMP_ULE;
+        if (opSymbol == ">") return LLIL_CMP_UGT;
+        if (opSymbol == ">=") return LLIL_CMP_UGE;
+        if (opSymbol == "x" || opSymbol == "y") return LLIL_REG;
+        
+        try {
+            size_t idx;
+            std::stoi(opSymbol, &idx);
+
+            if (idx == opSymbol.size())
+                return LLIL_CONST;
+                
+        } catch (const std::invalid_argument&) {}
+
+        return -1;
+    }
+
+    void patternsToString() {
+        std::cout << "Number of patterns loaded: " << patterns.size() << std::endl;
+        for (const auto& pattern : patterns) {
+            auto tokensFirst = tokenizeAndMapToLLIL(pattern.first);
+            auto tokensSecond = tokenizeAndMapToLLIL(pattern.second);
+
+            std::cout << "original " << pattern.first << " ([";
+            for (size_t i = 0; i < tokensFirst.size(); ++i) {
+                std::visit([](auto&& arg) {
+                    using T = std::decay_t<decltype(arg)>;
+                    if constexpr (std::is_same_v<T, int>) {
+                        std::cout << arg;
+                    } else if constexpr (std::is_same_v<T, std::vector<int>>) {
+                        std::cout << "[";
+                        for (size_t j = 0; j < arg.size(); ++j) {
+                            std::cout << arg[j];
+                            if (j != arg.size() - 1) std::cout << " ";
+                        }
+                        std::cout << "]";
+                    }
+                }, tokensFirst[i]);
+                if (i != tokensFirst.size() - 1) std::cout << ", ";
+            }
+
+            std::cout << "]) -> obfuscated " << pattern.second << " ([";
+            for (size_t i = 0; i < tokensSecond.size(); ++i) {
+                std::visit([](auto&& arg) {
+                    using T = std::decay_t<decltype(arg)>;
+                    if constexpr (std::is_same_v<T, int>) {
+                        std::cout << arg;
+                    } else if constexpr (std::is_same_v<T, std::vector<int>>) {
+                        std::cout << "[";
+                        for (size_t j = 0; j < arg.size(); ++j) {
+                            std::cout << arg[j];
+                            if (j != arg.size() - 1) std::cout << " ";
+                        }
+                        std::cout << "]";
+                    }
+                }, tokensSecond[i]);
+                if (i != tokensSecond.size() - 1) std::cout << ", ";
+            }
+            std::cout << "])" << std::endl;
+        }
+    }
 };
 
 #endif // METHODS_INSTRUCTIONS_MBA_SIMPLIFIER_HPP
--- src/methods/instructions/mbasimplifier.cpp
+++ src/methods/instructions/mbasimplifier.cpp
@@ -1,45 +1,154 @@
 #include "../../../include/methods/instructions/mbasimplifier.hpp"
 #include "../../../binaryninjaapi/lowlevelilinstruction.h"
 
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+
 using namespace BinaryNinja;
 
 MBASimplifier::MBASimplifier() : IDeobfuscationMethod("MBA Simplifier", DeobfuscationCategory::Workflow) {
     this->isEnabled = true;
+    std::string searchDir = "resources/";
+
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(searchDir)) {
+        if (entry.is_regular_file()) {
+            std::string filename = entry.path().filename().string();
+            if (filename.find("mba-dataset") != std::string::npos && entry.path().extension() == ".csv") {
+                loadPatternsFromCSV(entry.path().string());
+            }
+        }
+    }
+
+    patternsToString();
 }
 
-void MBASimplifier::execute(const Ref<AnalysisContext>& analysisContext)
+bool MBASimplifier::loadPatternsFromCSV(const std::string& csvFilePath)
+{ 
+    std::ifstream file(csvFilePath);
+    if (!file.is_open())
+        return {};
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (line.empty() || line[0] == '#' || line == "Original,Obfuscated")
+            continue;
+
+        std::stringstream ss(line);
+        std::string pattern, replacement;
+
+        if (!std::getline(ss, pattern, ','))
+            continue;
+
+        if (!std::getline(ss, replacement))
+            continue;
+        patterns.emplace_back(std::move(pattern), std::move(replacement));
+    }
+
+    return !patterns.empty();
+}
+std::vector<std::variant<int, std::vector<int>>> MBASimplifier::tokenizeAndMapToLLIL(const std::string& expr) const
 {
-    const Ref<Function> func = analysisContext->GetFunction();
+    std::vector<std::variant<int, std::vector<int>>> llilOps;
+    std::vector<std::vector<int>> stack;
+    std::vector<int> current;
+
+    std::string token;
+    size_t i = 0;
+    while (i < expr.size())
+    {
+        if (isspace(expr[i])) {
+            ++i;
+            continue;
+        }
+
+        if (expr[i] == '(') {
+            stack.push_back(current);
+            current.clear();
+            ++i;
+            continue;
+        }
+        if (expr[i] == ')') {
+            if (!stack.empty()) {
+                std::vector<int> group = current;
+                current = stack.back();
+                stack.pop_back();
+                llilOps.push_back(group);
+            }
+            ++i;
+            continue;
+        }
+
+        if (isalpha(expr[i])) {
+            size_t start = i;
+            while (i < expr.size() && (isalnum(expr[i]) || expr[i] == '_'))
+                ++i;
+            token = expr.substr(start, i - start);
+        } else {
+            token = expr.substr(i, 1);
+            ++i;
+        }
+
+        int op = mapSymbolToLLIL(token);
+        if (op != -1)
+            current.push_back(op);
+    }
+
+    if (!current.empty())
+        llilOps.push_back(current);
+
+    return llilOps;
+}
+
+std::vector<std::tuple<std::string, size_t, LowLevelILInstruction, LowLevelILInstruction, LowLevelILInstruction>> MBASimplifier::searchPatternsInFunction(const Ref<Function>& func) const
+{
+    std::vector<std::tuple<std::string, size_t, LowLevelILInstruction, LowLevelILInstruction, LowLevelILInstruction>> matches;
     const Ref<LowLevelILFunction> llil = func->GetLowLevelIL();
     if (llil)
     {
         for (Ref<BasicBlock>& block : llil->GetBasicBlocks())
         {
-            for (size_t instrIndex = block->GetStart(); instrIndex < block->GetEnd(); instrIndex++)
+            for (size_t instrIndex = block->GetStart(); instrIndex < block->GetEnd(); instrIndex++) 
             {
                 LowLevelILInstruction instr = (*llil)[instrIndex];
-                if (instr.operation == LLIL_SET_REG)
+                for (const auto& pattern : patterns)
                 {
-                    LowLevelILInstruction srcExpr = instr.GetSourceExpr<LLIL_SET_REG>();
-                    if (srcExpr.operation == LLIL_ADD)
-                    {
-                        LowLevelILInstruction leftExpr = srcExpr.GetLeftExpr<LLIL_ADD>();
-                        LowLevelILInstruction rightExpr = srcExpr.GetRightExpr<LLIL_ADD>();
-
-                        if (leftExpr.operation == LLIL_REG && rightExpr.operation == LLIL_REG)
-                        {
-                            const ExprId newInstr = llil->AddExpr(
-                                LLIL_MUL, srcExpr.size, srcExpr.flags,
-                                llil->Register(leftExpr.size, leftExpr.GetSourceRegister()),
-                                llil->Register(rightExpr.size, rightExpr.GetSourceRegister())
-                            );
-
-                            llil->ReplaceExpr(srcExpr.exprIndex, newInstr);
-                            llil->GenerateSSAForm();
-                        }
-                    }
+                    // TODO
                 }
             }
         }
     }
+
+    return matches;
+}
+
+size_t MBASimplifier::replaceObfuscatedWithSimple(const Ref<LowLevelILFunction>& llil,
+    const LowLevelILInstruction& srcExpr,
+    const LowLevelILInstruction& leftExpr,
+    const LowLevelILInstruction& rightExpr)
+{
+    ExprId newInstr = llil->AddExpr(
+        LLIL_MUL, srcExpr.size, srcExpr.flags,
+        llil->Register(leftExpr.size, leftExpr.GetSourceRegister()),
+        llil->Register(rightExpr.size, rightExpr.GetSourceRegister())
+    );
+
+    llil->ReplaceExpr(srcExpr.exprIndex, newInstr);
+    llil->GenerateSSAForm();
+
+    return 1;
+}
+
+void MBASimplifier::execute(const Ref<AnalysisContext>& analysisContext)
+{
+    // const Ref<Function> func = analysisContext->GetFunction();
+    // auto matches = searchPatternsInFunction(func);
+    // const Ref<LowLevelILFunction> llil = func->GetLowLevelIL();
+
+    // for (const auto& match : matches)
+    // {
+    //     const auto& [pattern, instrIndex, srcExpr, leftExpr, rightExpr] = match;
+    //     replaceObfuscatedWithSimple(llil, srcExpr, leftExpr, rightExpr);
+    // }
 }

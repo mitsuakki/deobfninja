65da2fa2950abeb40335398cc4f8e65dc7017b93
Alexis Daugé <alexis.dauge2004@gmail.com>
2025-07-23 11:11:40 +0200

    delete useless header functions declarations

--- include/methods/flows/nativepredicatesolver.hpp
+++ include/methods/flows/nativepredicatesolver.hpp
@@ -20,23 +20,7 @@ namespace Flows {
     class NativePredicateSolver : public IDeobfuscationMethod {
     public:
         NativePredicateSolver();
-        
         void execute(const BinaryNinja::Ref<BinaryNinja::AnalysisContext>& analysisContext) override;
-        
-        // Method to patch a single function (useful for testing or targeted patching)
-        int patchSingleFunction(const BinaryNinja::Ref<BinaryNinja::BinaryView>& view, 
-                               const BinaryNinja::Ref<BinaryNinja::Function>& func,
-                               int maxPasses = 10);
-
-    private:
-        // Helper method to process a batch of functions in a worker thread
-        void processFunctionBatch(BinaryNinja::Ref<BinaryNinja::BinaryView> viewRef, 
-                                 const std::vector<BinaryNinja::Ref<BinaryNinja::Function>>& funcBatch,
-                                 int maxPassesPerFunction,
-                                 std::atomic<int>& patchCount,
-                                 std::atomic<bool>& shouldCancel,
-                                 std::mutex& updateMutex,
-                                 std::atomic<size_t>& processedFunctions);
     };
 }
 
